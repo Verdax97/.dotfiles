5853deca269927a6137bfaff6cb01a3095b83627
Nicolò Sonnino <10569180@polimi.it>
2020-08-08 13:30:56 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ sudo apt upgrade
 sudo apt install build-essential git wget curl valgrind gdb gcc tree openssh-client openssh-server kcachegrind python3 python3-pip htop aria2 ffmpeg cmake g++ maven dnsutils gzip unzip zsh ruby-dev neofetch lolcat cowsay fortune toilet figlet
 
 # Install starship.rs
-sudo curl -fsSL https://starship.rs/install.sh | bash
+sudo curl -fsSL https://starship.rs/install.sh | sudo bash
 
 # Install colorls
 sudo gem install colorls
