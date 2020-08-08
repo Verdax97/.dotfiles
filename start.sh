8177019b451f43e4947270b294006197d521f12c
Nicolò Sonnino <10569180@polimi.it>
2020-08-08 13:36:21 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,6 +17,12 @@ pip3 install youtube-dl
 # Set figlet/toilet font
 sudo cp ANSI\ Shadow.flf /usr/share/figlet
 
+# Set zsh-substring-search-history
+git clone https://github.com/zsh-users/zsh-history-substring-search.git
+cd zsh-history-substring-search/
+sudo mkdir /usr/local/share/zsh-history-substring-search
+sudo cp zsh-history-substring-search.zsh /usr/local/share/zsh-history-substring-search
+
 # Update current terminal
 cd
 source .zshrc
